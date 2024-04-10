01b51bcb05d41080d56e472865021f6dcb10cc7f
Rajesh Pillai <pillai.rajesh@gmail.com>
2024-04-10 03:35:03 +0000

    Replaced setInterval with requestAnimationFrame basic version

--- game-template/game.js
+++ game-template/game.js
@@ -1,4 +1,13 @@
-window.onload = () => {
+//the coordinates of the block
+let x = 0
+let y = 0
+
+//the vertical velocity of the block
+let yvel = 0
+
+document.addEventListener("DOMContentLoaded", onLoad, false);
+
+function onLoad() {
 
     //input is the array of keys currently pressed down
     input = []
@@ -17,44 +26,40 @@ window.onload = () => {
             input.splice(i,1)
         }
     })
+    
+    requestAnimationFrame(animate);
+}
 
-    //the coordinates of the block
-    x = 0
-    y = 0
-
-    //the vertical velocity of the block
-    yvel = 0
-
+function animate() {
     //this is the update function
-    setInterval(() => {
+    
+    //this is the gravity logic
+    if(y+yvel<0) {
+        y = 0
+        yvel = 0
+    } else if(y>0){
+        yvel -= 0.25
+        y += yvel
+    } else {
+        yvel = 0
+        y = 0
+    }
 
-        //this is the gravity logic
-        if(y+yvel<0) {
-            y = 0
-            yvel = 0
-        } else if(y>0){
-            yvel -= 0.25
-            y += yvel
-        } else {
-            yvel = 0
-            y = 0
-        }
+    //this updates coordinates and velocity based on input
+    if(input.includes("w") && y == 0) {
+        yvel = 3
+        y+=yvel
+    }
+    if(input.includes("a")) {
+        x -= 1
+    }
+    if(input.includes("d")) {
+        x += 1
+    }
+    
+    //this updates the css for the block based on coordinates
+    document.getElementById("char").style.left = "min(" + x + "vw, " + x + "vh)";
+    document.getElementById("char").style.bottom = "min(" + y + "vw, " + y + "vh)";
 
-        //this updates coordinates and velocity based on input
-        if(input.includes("w") && y == 0) {
-            yvel = 3
-            y+=yvel
-        }
-        if(input.includes("a")) {
-            x -= 1
-        }
-        if(input.includes("d")) {
-            x += 1
-        }
-        
-        //this updates the css for the block based on coordinates
-        document.getElementById("char").style.left = "min(" + x + "vw, " + x + "vh)"
-        document.getElementById("char").style.bottom = "min(" + y + "vw, " + y + "vh)"
-    }, 1000/60)
-
+    requestAnimationFrame(animate);
 }
